f293af52bf2591583d331f9acd85d0a2f8fbfd0a
Dan <your_dkwagner@mtu.edu>
2019-04-28 21:55:08 -0500

    Inital commit

---
+++ src/main/kotlin/com/stalagbyte/TestApplication.kt
@@ -0,0 +1,11 @@
+package com.stalagbyte
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestApplication
+
+fun main(args: Array<String>){
+    runApplication<TestApplication>(*args)
+}
